441a8866e5ed32f338770dc31e5a5d90aaf2b69f
vagif279 <vagif.gambarov2015@gmail.com>
2024-03-06 20:27:40 +0400

    Optimistic UI

--- React_Router/src/routes/contact.jsx
+++ React_Router/src/routes/contact.jsx
@@ -83,6 +83,11 @@ function Favorite({ contact }) {
     const fetcher = useFetcher();
     // yes, this is a `let` for later
     let favorite = contact.favorite;
+
+    if (fetcher.formData) {
+        favorite = fetcher.formData.get("favorite") === "true";
+    }
+    
     return (
         <fetcher.Form method="post">
             <button
