8bc31252069dde9837ea5e640a00b4e114852a15
vagif279 <vagif.gambarov2015@gmail.com>
2024-03-06 17:11:58 +0400

    Global Pending UI

--- React_Router/src/routes/root.jsx
+++ React_Router/src/routes/root.jsx
@@ -1,10 +1,10 @@
 import { 
     Outlet, 
     NavLink,
-    Link,
     useLoaderData,
     Form,
     redirect,
+    useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -20,6 +20,8 @@ export async function loader() {
 
 export default function Root() {
     const { contacts } = useLoaderData();
+    const navigation = useNavigation();
+
     return (
       <>
         <div id="sidebar">
@@ -81,8 +83,13 @@ export default function Root() {
             )}
           </nav>
         </div>
-        <div id="detail">
-            <Outlet />
+        <div 
+          id="detail"
+          className={
+            navigation.state === "loading" ? "loading" : ""
+          }
+        >
+          <Outlet />
         </div>
       </>
     );
